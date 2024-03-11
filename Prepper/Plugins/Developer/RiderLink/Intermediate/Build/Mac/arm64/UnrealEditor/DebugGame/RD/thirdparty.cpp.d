34c555a747063b614e23cfa00469d990afaad2b7
EasyH <61585427+Easy-H@users.noreply.github.com>
2024-03-11 22:13:18 +0900

    gitignore 오타 수정

--- Prepper/Plugins/Developer/RiderLink/Intermediate/Build/Mac/arm64/UnrealEditor/DebugGame/RD/thirdparty.cpp.d
+++
@@ -1,3 +0,0 @@
-/Users/jangbogwang/Documents/GitHub/unreal/Prepper/Plugins/Developer/RiderLink/Intermediate/Build/Mac/arm64/UnrealEditor/DebugGame/RD/thirdparty.cpp.o: \
-  /Users/jangbogwang/Documents/GitHub/unreal/Prepper/Plugins/Developer/RiderLink/Source/RD/thirdparty/thirdparty.cpp \
-  /Users/jangbogwang/Documents/GitHub/unreal/Prepper/Plugins/Developer/RiderLink/Intermediate/Build/Mac/arm64/UnrealEditor/DebugGame/RD/Definitions.h
