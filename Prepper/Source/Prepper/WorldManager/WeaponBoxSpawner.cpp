0b2b7df7064028c6f0f2ce86e01d129bcf811578
bokwang jang <101097018+B0Light@users.noreply.github.com>
2024-10-17 20:24:49 +0900

    FIX Weapon Drop Box

--- Prepper/Source/Prepper/WorldManager/WeaponBoxSpawner.cpp
+++ Prepper/Source/Prepper/WorldManager/WeaponBoxSpawner.cpp
@@ -1,8 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "WeaponBoxSpawner.h"
-
+#include "Net/UnrealNetwork.h"
 #include "Prepper/Item/Object/InteractableBox.h"
 
 // Sets default values
@@ -10,33 +7,55 @@ AWeaponBoxSpawner::AWeaponBoxSpawner()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	FallingSpeed = 100.0f;
+	bReplicates = true;
+}
+
+void AWeaponBoxSpawner::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AWeaponBoxSpawner, ControlledFallingActor);
+}
+
+void AWeaponBoxSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	if (HasAuthority())
+	{
+		SpawnFallingActor_Server();
+	}
 }
 
 void AWeaponBoxSpawner::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-void AWeaponBoxSpawner::SpawnFallingActor()
+void AWeaponBoxSpawner::SpawnFallingActor_Server_Implementation()
 {
 	if (FallingActorClass)
 	{
-		// 스폰할 위치 설정
 		FVector SpawnLocation = GetActorLocation();
 		SpawnLocation.Z += SpawnHeight;
 
 		FRotator SpawnRotation = FRotator::ZeroRotator;
 
 		ControlledFallingActor = GetWorld()->SpawnActor<AInteractableBox>(FallingActorClass, SpawnLocation, SpawnRotation);
-		ControlledFallingActor->BoxMesh->SetSimulatePhysics(true);
-		ControlledFallingActor->BoxMesh->SetEnableGravity(true);
+		if (ControlledFallingActor)
+		{
+			ControlledFallingActor->SetReplicates(true);
+			Multicast_DropActor();
+		}
 	}
 }
 
-void AWeaponBoxSpawner::BeginPlay()
+void AWeaponBoxSpawner::Multicast_DropActor_Implementation()
 {
-	Super::BeginPlay();
-	SpawnFallingActor();
-}
+	if (ControlledFallingActor && ControlledFallingActor->BoxMesh)
+	{
+		ControlledFallingActor->BoxMesh->SetSimulatePhysics(true);
+		ControlledFallingActor->BoxMesh->SetEnableGravity(true);
 
+		// 클라이언트에서도 물리 시뮬레이션이 활성화되도록 클라이언트 함수 호출
+		ControlledFallingActor->BoxMesh->SetIsReplicated(true);
+	}
+}
